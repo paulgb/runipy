f1d5c179cfdd07fec0edf3928be762740e15f53b
Paul Butler <paulgb@gmail.com>
2014-03-20 10:27:07 -0400

    tests for SVG mime type

--- test.py
+++ test.py
@@ -2,18 +2,34 @@
 import unittest
 from glob import glob
 from os import path
+import re
 
 from IPython.nbformat.current import read
 
 from runipy.notebook_runner import NotebookRunner
 
 class TestRunipy(unittest.TestCase):
-    maxDiff = None
+    def prepare_cell(self, cell):
+        cell = dict(cell)
+        if 'metadata' in cell:
+            del cell['metadata']
+        if 'text' in cell:
+            cell['text'] = re.sub('0x[0-9a-f]{9}', '<HEXADDR>', cell['text'])
+        return cell
+
+
     def assert_notebooks_equal(self, expected, actual):
         self.assertEquals(len(expected['worksheets'][0]['cells']), len(actual['worksheets'][0]['cells']))
         for expected_out, actual_out in zip(expected['worksheets'][0]['cells'],
                 actual['worksheets'][0]['cells']):
-            self.assertEquals(dict(expected_out), dict(actual_out))
+            for k in set(expected_out).union(actual_out):
+                if k == 'outputs':
+                    self.assertEquals(len(expected_out[k]), len(actual_out[k]))
+                    for e, a in zip(expected_out[k], actual_out[k]):
+                        e = self.prepare_cell(e)
+                        a = self.prepare_cell(a)
+                        self.assertEquals(e, a)
+                    
 
     def testRunNotebooks(self):
         input_glob = path.join('tests', 'input', '*.ipynb')
