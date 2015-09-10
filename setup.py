20b13f8757c7fe9ad97c18d00179ff5e08d4dcfc
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-10 14:13:16 -0400

    setup.py: Make sure we don't include iPython 4 when building as it is not currently supported.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='runipy',
       install_requires=[
           'Jinja2>=2.7.2',
           'Pygments>=1.6',
-          'ipython>=2.3.1',
+          'ipython>=2.3.1,<4',
           'pyzmq>=14.1.0',
       ],
       packages=['runipy'],
