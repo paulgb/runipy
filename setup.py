cd768193b41de1402f75435dd6e225cbfce0b4f5
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-10 20:05:07 -0400

    setup.py: Fix indentation.

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from setuptools import setup
 import runipy
 
 setup(name = 'runipy',
-    version = runipy.__version__,
-    description = 'Run IPython notebooks from the command line',
-    url = 'https://github.com/paulgb/runipy',
-    author = 'Paul Butler',
-    author_email = 'paulgb@gmail.com',
-    classifiers = [
-        'Framework :: IPython',
-    ],
-    install_requires = [
-        'Jinja2>=2.7.2',
-        'Pygments>=1.6',
-        'ipython>=2.3.1',
-        'pyzmq>=14.1.0',
-    ],
-    packages = ['runipy'],
-    entry_points = {
-        'console_scripts': [
-            'runipy = runipy.main:main'
-        ]
-    },
+      version = runipy.__version__,
+      description = 'Run IPython notebooks from the command line',
+      url = 'https://github.com/paulgb/runipy',
+      author = 'Paul Butler',
+      author_email = 'paulgb@gmail.com',
+      classifiers = [
+          'Framework :: IPython',
+      ],
+      install_requires = [
+          'Jinja2>=2.7.2',
+          'Pygments>=1.6',
+          'ipython>=2.3.1',
+          'pyzmq>=14.1.0',
+      ],
+      packages = ['runipy'],
+      entry_points = {
+          'console_scripts': [
+              'runipy = runipy.main:main'
+          ]
+      },
 )
